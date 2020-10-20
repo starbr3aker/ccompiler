eb183dd5ce18b893e97e190bc0dd455853ba2f38
Nuthan Kumar S <nuthankumars10@gmail.com>
2020-10-20 18:37:33 +0530

    test 2 and 10

---
+++ semantic analyser/testcases/test10.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+int main()
+{
+  int _var;
+  int ghi0 = 1;
+  char str[100] = "Hello world";
+  int x[] = {0, 1, 2};
+	int 5abc = 10;
+}
---
+++ semantic analyser/testcases/test2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//test case for simple expressions
+
+void main(){
+	int a,b,c,d,e,f,g,h;
+
+  a=2;
+  b=3;
+  a = a+b;
+  c=a++;
+  d=a-b;
+  h += a;
+  e=(d+c)*a;
+  f=++(g/h)+b;
+}
